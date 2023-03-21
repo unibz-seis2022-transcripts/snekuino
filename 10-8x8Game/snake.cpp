c241c6ee2ffd8d10cfa9dccbc30c44e08722fd0f
Fabio Solaroli <fabio@solaroli.it>
2023-03-21 10:59:20 +0100

    Implement createSnake

--- 10-8x8Game/snake.cpp
+++ 10-8x8Game/snake.cpp
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BASE_SPEED 2000
+#define STARTING_POSITION { 0, 0 }
+
+int rows = 0;
+int cols = 0;
+
+struct body* allocBody(struct position pos) {
+	struct body* body = (struct body*)malloc(sizeof(struct body));
+	if (body != NULL) {
+		body->pos = pos;
+		body->next = NULL;
+	}
+	return body;
+}
 struct snake* createSnake(int r, int c) {
 	struct snake* snake = (struct snake*)malloc(sizeof(struct snake));
+	if (snake != NULL) {
+		snake->body = allocBody(STARTING_POSITION);
+		snake->direction = { 1, 0 };
+		snake->speed = BASE_SPEED;
+	}
+	rows = r;
+	cols = c;
 	return snake;
 }
 
