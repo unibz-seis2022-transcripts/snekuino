a6e6a101f38a01ebd586dc7c5e3d205496a2a8bb
LinusAlbert.Scheibe <lscheibe@unibz.it>
2023-04-04 11:47:31 +0200

    numbers WIP

---
+++ 10-8x8Game/numbers.cpp
@@ -0,0 +1,38 @@
+#include "numbers.h"
+
+position numberOnePixels[] = {
+	{ 2, 4 },
+	{ 1, 3 },
+	{ 2, 3 },
+	{ 2, 2 },
+	{ 2, 1 },
+	{ 2, 0 },
+};
+
+position numberTwoPixels[] = {
+	{ 0, 4 },
+	{ 1, 4 },
+	{ 2, 3 },
+	{ 1, 2 },
+	{ 0, 1 },
+	{ 0, 0 },
+	{ 0, 1 },
+	{ 0, 2 },
+};
+
+position numberThreePixels[] = {
+	{ 0, 4 },
+	{ 1, 4 },
+	{ 2, 4 },
+	{ 2, 3 },
+	{ 1, 2 },
+	{ 2, 1 },
+	{ 1, 0 },
+	{ 0, 0 },
+};
+
+position numberFourPixels[] = {
+	{ 2, 4 },
+	{ 1, 3 },
+	{  },
+};
---
+++ 10-8x8Game/numbers.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "snake.h"
+
+const position numberOnePixels[];
+const position numberTwoPixels[];
+const position numberThreePixels[];
+const position numberFourPixels[];
+const position numberFivePixels[];
+const position numberSixPixels[];
+const position numberSevenPixels[];
+const position numberEightPixels[];
+const position numberNinePixels[];
+const position numberZeroPixels[];
