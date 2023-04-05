f113d63f2b758cf0b9d52bad21f67b5901b3f4e3
LinusAlbert.Scheibe <lscheibe@unibz.it>
2023-04-05 15:40:08 +0200

    print left and right digits

--- 10-8x8Game/numbers.cpp
+++ 10-8x8Game/numbers.cpp
@@ -1,4 +1,6 @@
 #include "numbers.h"
+#include <stdlib.h>
+#include "pixelhelper.h"
 
 position numberOnePixels[] = {
 	{ 2, 4 },
@@ -121,3 +123,58 @@ position numberZeroPixels[] = {
 	{ 1, 4 },
 	{ 2, 4 },
 };
+
+position* digitPixelArrays[] = {
+	numberZeroPixels,
+	numberOnePixels,
+	numberTwoPixels,
+	numberThreePixels,
+	numberFourPixels,
+	numberFivePixels,
+	numberSixPixels,
+	numberSevenPixels,
+	numberEightPixels,
+	numberNinePixels,
+};
+
+struct position* translatePixels(struct position* pixels, int lengthPixels, int translateX, int translateY) {
+	struct position* translatedPositions = (struct position*) malloc(lengthPixels * sizeof(struct position));
+	for (int i = 0; i < lengthPixels; i++) {
+		translatedPositions[i].x = pixels[i].x + translateX;
+		translatedPositions[i].y = pixels[i].y + translateY;
+	}
+	return translatedPositions;
+};
+
+void printNumber(int value) {
+	// Make sure to only print values < 100
+	int twoDigitValue = value % 100;
+	int leftDigit = (value / 10) % 10;
+	int rightDigit = value % 10;
+	printLeftDigit(leftDigit);
+	printRightDigit(rightDigit);
+}
+
+void printLeftDigit(int value) {
+	const int translateX = 0;
+	const int translateY = 1;
+
+	int digit = value % 10;
+	struct position* pixels = digitPixelArrays[digit];
+	int lengthPixels = sizeof(pixels) / sizeof(pixels[0]);
+
+	struct position* translatedPixels = translatePixels(pixels, lengthPixels, translateX, translateY);
+	drawPixels(translatedPixels, lengthPixels);
+}
+
+void printRightDigit(int value) {
+	const int translateX = 4;
+	const int translateY = 1;
+
+	int digit = value % 10;
+	struct position* pixels = digitPixelArrays[digit];
+	int lengthPixels = sizeof(pixels) / sizeof(pixels[0]);
+
+	struct position* translatedPixels = translatePixels(pixels, lengthPixels, translateX, translateY);
+	drawPixels(translatedPixels, lengthPixels);
+}
--- 10-8x8Game/numbers.h
+++ 10-8x8Game/numbers.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "snake.h"
 
-const position numberOnePixels[];
-const position numberTwoPixels[];
-const position numberThreePixels[];
-const position numberFourPixels[];
-const position numberFivePixels[];
-const position numberSixPixels[];
-const position numberSevenPixels[];
-const position numberEightPixels[];
-const position numberNinePixels[];
-const position numberZeroPixels[];
+position numberOnePixels[];
+position numberTwoPixels[];
+position numberThreePixels[];
+position numberFourPixels[];
+position numberFivePixels[];
+position numberSixPixels[];
+position numberSevenPixels[];
+position numberEightPixels[];
+position numberNinePixels[];
+position numberZeroPixels[];
+
+position* digitPixelArrays[];
+
+void printNumber(int value);
+void printLeftDigit(int value);
+void printRightDigit(int value);
